153ee7b44ccd7b12a417d56b8b5d3c181ee77744
leompf <lmfraqueiro@gmail.com>
2025-07-08 00:30:19 +0100

    Create database and countries table

---
+++ SalesCet95.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesCet95.Web.Data.Entities;
+
+namespace SalesCet95.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
